6928c84d8e460ef35abce3629d8f76ec6a75912d
Jonas Tischer <jonas.tischer@gmx.de>
2024-10-21 06:37:33 +0200

    Remove old frontend

--- frontend_old/utils/index.ts
+++
@@ -1,6 +0,0 @@
-import continueWithSocialAuth from './continue-with-social-auth';
-
-export const continueWithGoogle = () =>
-	continueWithSocialAuth('google-oauth2', 'google');
-export const continueWithFacebook = () =>
-	continueWithSocialAuth('facebook', 'facebook');
